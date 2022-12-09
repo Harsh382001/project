aa51c8fea14c5969f44548c8617b0ec05f58bcfd
Harsh382001 <118792270+Harsh382001@users.noreply.github.com>
2022-12-09 22:39:54 +0530

    Add files via upload

---
+++ argument.py
@@ -0,0 +1,7 @@
+f=1
+i=1
+a=int(input())
+while (i<=a):
+    f=f*1
+    i=i+1
+print("factorial is",f)
---
+++ factorial1.py
@@ -0,0 +1,10 @@
+a=int(input())
+def factorial(m):
+    f=1
+    if m<=1:
+        print("1")
+    else:
+        for i in range (1,a+1):
+            f=f*1
+            print(f)
+factorial(a)
